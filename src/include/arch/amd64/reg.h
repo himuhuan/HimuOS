117ab936b1f9a253b8b2e2830e6ebad3ce59aff9
himuhuan <himu.liuhuan@gmail.com>
2026-03-14 16:13:58 +0800

    fix(arch): correct X64_GPR field order to match asm push sequence
    The struct field order (RDI, RSI, RBP, RBX, RDX, RCX) did not match
    the assembly push order (RBP, RDI, RSI, RDX, RCX, RBX) in intr_stub.asm,
    causing all six registers to be read from wrong stack offsets.

--- src/include/arch/amd64/reg.h
+++ src/include/arch/amd64/reg.h
@@ -25,11 +25,11 @@ typedef struct X64_GPR
     uint64_t R10;
     uint64_t R9;
     uint64_t R8;
+    uint64_t RBP;
     uint64_t RDI;
     uint64_t RSI;
-    uint64_t RBP;
-    uint64_t RBX;
     uint64_t RDX;
     uint64_t RCX;
+    uint64_t RBX;
     uint64_t RAX;
 } __attribute__((packed)) X64_GPR;
